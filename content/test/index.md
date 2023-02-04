abb64c88a8b211f32357de01bee1b82275a982d3
njy1213 <njy1213@njy1213-Macbook.local>
2023-02-04 20:59:48 +0900

    feat: init  Setting

---
+++ content/test/index.md
@@ -0,0 +1,12 @@
+---
+emoji: 🧢
+title: Getting Started
+date: '2023-02-04 20:00:00'
+author: 남준영
+tags: tutorial
+categories: tutorial
+---
+
+
+```toc
+```
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `jun7867`,
+  description: `Nam IT 블로그`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://jun7867.github.io/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `jun7867/jun7867.github.io`,
     },
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `남준영`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
-      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+      description: ['가치를 만드는', '끝까지 가는'],
+      thumbnail: 'img_io.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/jun7867`,
+      linkedIn: ``,
+      email: `jjune095@gmail.com`,
     },
   },
 
@@ -43,13 +43,8 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
+        date: '2021.08 ~',
+        activity: 'Midas Jobda Mobile 개발',
       },
     ],
 
